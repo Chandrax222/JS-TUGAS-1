12072a1a81f29ceb2cd4cbf32e12a07be32215c4
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-09 22:12:55 +0700

    var, let, const dan log
    mengenal var, let, const dan log

---
+++ Tugas1.js
@@ -0,0 +1,8 @@
+var nama_depan = "Chandra";
+let nama_akhir = "Tera";
+const umur = 22;
+
+console.log(nama_depan);
+console.log(nama_akhir);
+console.log(umur);
+console.log(`Nama Saya : ${nama_depan }${nama_akhir}${umur}`);
